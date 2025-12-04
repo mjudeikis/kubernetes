2d3844cec735a2574642083ac1ab788b786264f4
Mangirdas Judeikis <mangirdas@judeikis.lt>
2025-12-04 15:16:14 +0200

    protoc

--- pkg/controlplane/apiserver/config.go
+++ pkg/controlplane/apiserver/config.go
@@ -143,11 +143,13 @@ func BuildGenericConfig(
 		return
 	}
 
+	// kcp: disable protobuf for loopback as we can't ensure every server in the chain will support this.
 	// Use protobufs for self-communication.
 	// Since not every generic apiserver has to support protobufs, we
 	// cannot default to it in generic apiserver and need to explicitly
 	// set it in kube-apiserver.
-	genericConfig.LoopbackClientConfig.ContentConfig.ContentType = "application/vnd.kubernetes.protobuf"
+	// genericConfig.LoopbackClientConfig.ContentConfig.ContentType = "application/vnd.kubernetes.protobuf"
+
 	// Disable compression for self-communication, since we are going to be
 	// on a fast local network
 	genericConfig.LoopbackClientConfig.DisableCompression = true
